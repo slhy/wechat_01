55c4430b9e33ef96f236489b92038111ba8b0b00
slhy <824076046@qq.com>
2016-10-20 12:36:09 +0800

    初始化项目

---
+++ pages/friend/friend.js
@@ -0,0 +1,8 @@
+//friend.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'friend'
+  }
+})
---
+++ pages/user/user.js
@@ -0,0 +1,8 @@
+//user.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'user'
+  }
+})
